461cc9de93243633a1ea3b7eeb1a86837fca2c68
Vijay Kumar T <107529764+Vijaykumart8896@users.noreply.github.com>
2022-11-29 12:29:40 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const { MONGODB_URL, MONGODB_CLOUD } = require("./index");
 
 let connectDataBase = async () => {
     try {
-        await connect(MONGODB_CLOUD);
+        await connect("mongodb+srv://manasa:vijay123@cluster0.nkf6fc8.mongodb.net/?retryWrites=true&w=majority");
         success("MongoDB database is connected!")
     } catch (err) {
         error(err)
